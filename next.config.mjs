97aa6f735e699f7b9c9bba94feaa840e369ee496
Leo Nwoke <leonnwoke@gmail.com>
2024-03-09 17:55:08 -0600

    Added comment for Node deployment

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   images: {
+
+    //Delete these 2 loader lines and the loader.js file if you want to deploy on a Node server like Vercel
     loader: "custom",
     loaderFile: "./loader.js",
     
